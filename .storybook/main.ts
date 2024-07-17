1e2b9d935e8ea2a261aca621124555de94fffb92
jSchoenaers <julien.schoenaers@gmail.com>
2024-07-17 15:13:48 +0200

    Setup

---
+++ .storybook/main.ts
@@ -0,0 +1,27 @@
+import type { StorybookConfig } from '@storybook/react-vite';
+
+const config : StorybookConfig  = {
+  stories: ["../src/stories/**/*.mdx", "../src/stories/**/*.stories.@(js|jsx|ts|tsx)"],
+
+  addons: [
+    "@storybook/addon-onboarding",
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+    "@storybook/addon-interactions",
+    "@chromatic-com/storybook",
+    "@storybook/addon-actions"
+  ],
+
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+
+  docs: {
+    autodocs: true
+  }
+};
+
+
+export default config;
---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}", "./stories/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      primary: {
+        50: "#ECE5FA",
+        100: "#D9CBF5",
+        200: "#B79CEC",
+        300: "#9169E3",
+        400: "#6F39DA",
+        500: "#5422B5",
+        600: "#431B92",
+        700: "#31146B",
+        800: "#210E49",
+        900: "#100622",
+        950: "#080311",
+      },
+      white : "#FFFFFF",
+      glow:'#920DC6'
+    },
+    extend: {
+      dropShadow: {
+        glow: '0px 0px 4px rgba(111,57,218,0.8)'
+      }
+    },
+  },
+  plugins: [],
+};
